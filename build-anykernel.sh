9780a98c80da77c653d2a6fc94bc117c68fc05b1
kondors1995 <ronaldsustinovs@gmail.com>
2017-02-09 15:18:11 +0200

    Fix

--- build-anykernel.sh
+++ build-anykernel.sh
@@ -88,7 +88,7 @@ echo "Render Kernel Creation Script:"
 echo -e "${restore}"
 
 echo "Pick Toolchain..."
-select choice in LINARO-aarch64-linux-gnu-4.9.3-05012016 LINARO-aarch64-linux-gnu-5.3.1-05012016 HYPER-aarch64-6.x-10032016 UBER-aarch64-linux-gnu-6.x.
+select choice in LINARO-aarch64-linux-gnu-4.9.3-05012016 LINARO-aarch64-linux-gnu-5.3.1-05012016 HYPER-aarch64-6.x-10032016 UBER-aarch64-6.x.
 do
 case "$choice" in
 	"LINARO-aarch64-linux-gnu-4.9.3-05012016")
@@ -100,8 +100,8 @@ case "$choice" in
 	"HYPER-aarch64-6.x-10032016")
 		export CROSS_COMPILE=${HOME}/android/source/toolchains/HYPER-aarch64-6.x-10032016/bin/aarch64-linux-android-
 		break;;
-	"UBER-aarch64-linux-gnu-6.x.")
-		export CROSS_COMPILE=${HOME}/Android/Kernel/toolchains/aarch64-linux-android-6.x/bin/aarch64-linux-gnu-
+	"UBER-aarch64-6.x.")
+		export CROSS_COMPILE=${HOME}/Android/Kernel/toolchains/aarch64-linux-android-6.x/bin/aarch64-linux-android-
 		break;;
 esac
 done
